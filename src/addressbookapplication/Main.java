2b303911aada92fa6dde495df73cf0293d9d600d
Sterling1111 <sjeppson@horizon.csueastbay.edu>
2022-03-04 17:53:10 -0800

    added an int

--- src/addressbookapplication/Main.java
+++ src/addressbookapplication/Main.java
@@ -2,9 +2,11 @@ package addressbookapplication;
 
 public class Main {
 
+
     public static void main(String[] args) {
 	// write your code here
         System.out.println("high back"); //lynne added
         System.out.println("high back"); //lynne added
+        int a = 1;
     }
 }
