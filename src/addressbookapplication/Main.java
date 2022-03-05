fe099cc7ec52a950300bc358ba4c39666d0bf1f9
Sterling1111 <sjeppson@horizon.csueastbay.edu>
2022-03-04 17:46:47 -0800

    changed the print statment

--- src/addressbookapplication/Main.java
+++ src/addressbookapplication/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hello"); //lynne added
+        System.out.println("high back"); //lynne added
     }
 }
